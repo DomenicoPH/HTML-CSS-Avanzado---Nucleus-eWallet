87afc279a23815fda5056e066bcefe80458577b0
Domenico Pagano <dpaganoh@gmail.com>
2025-10-19 15:54:29 -0500

    header animations

--- js/script.js
+++ js/script.js
@@ -13,4 +13,48 @@ document.addEventListener("DOMContentLoaded", (event) => {
         scrub: true,
       }
   });
+
+  // logo izq. header
+  gsap.fromTo('.header__logo, .header__texto', {
+    opacity: 0,
+    scale: 0.8,
+    //x: -200
+  },{
+    //x: 0,
+    scale: 1,
+    opacity: 1,
+    duration: 1,
+    ease: 'sine'
+  })
+
+  // navegacion__link
+  gsap.from('.navegacion__link', {
+    y: -100,
+    ease: 'sine',
+    delay: .5,
+    duration: .8,
+    stagger: .2
+  })
+
+  // imagen del celular en el header
+  gsap.fromTo('.header__grafico', {
+    x: 800,
+    rotation: 15,
+  },{
+    x: 0,
+    rotation: 0,
+    duration: 1.5,
+    ease: 'power4.out',
+  })
+
+  // imagen de fondo del header
+  gsap.fromTo('.header',{
+    backgroundPosition: '200% 0%'
+  }, {
+    backgroundPosition: '100% 0%',
+    duration: 1,
+    ease: 'power2.out'
+  })
+
+
 });
